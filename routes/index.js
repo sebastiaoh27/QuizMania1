78c9d718a13b91a9307f579996ee742d78f8c4e3
sholtreman <sebastiaoh27@gmail.com>
2021-05-02 20:26:15 +0200

    update for deploy

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,30 @@
 var express = require('express');
 var router =express.Router();
+var url = "QuizMania/public/"
 
+router.get(url+'/', function (req,res,next) {
 
-router.get('/', function (req,res,next) {
+    console.log(url)
     res.sendFile("index.html",{root: "./public"})
 })
 
-router.get('/createQuiz', function (req,res,next) {
+router.get(url+'/createQuiz', function (req,res,next) {
     res.sendFile("createQuiz.html",{root: "./public"})
 })
 
-router.get('/contact', function (req,res,next) {
+router.get(url+'/contact', function (req,res,next) {
     res.sendFile("contact.html",{root: "./public"})
 })
 
-router.get('/createRoom', function (req,res,next) {
+router.get(url+'/createRoom', function (req,res,next) {
     res.sendFile("createRoom.html",{root: "./public"})
 })
 
-router.get('/room', function (req,res,next) {
+router.get(url+'/room', function (req,res,next) {
     res.sendFile("room.html",{root: "./public"})
 })
 
-router.get('/hostRoom', function (req,res,next) {
+router.get(url+'/hostRoom', function (req,res,next) {
     res.sendFile("hostRoom.html",{root: "./public"})
 })
 
